6e7927e6f142f113cce1f64b27dba8dc825451df
SebastiaanYN <sebastiaanyn@gmail.com>
2022-01-08 02:23:16 +0100

    Add interpreter

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 fn main() {
     let mut builder = cc::Build::new();
-    builder.file("src/brainfuck.S");
+
+    if cfg!(feature = "interpreter") {
+        builder.file("src/brainfuck_interpreter.S");
+    } else {
+        builder.file("src/brainfuck.S");
+    }
 
     if std::env::var("PROFILE").unwrap() == "debug" {
         builder.define("DEBUG", None);
